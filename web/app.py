efd2d02db8e5911a283155f2c92753da4c138d23
Evie Mo <wanyingmo97@gmail.com>
2022-12-18 02:35:16 -0800

    web version

--- web/app.py
+++ web/app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+from web.utils import validate_user_input
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def game_mode_selection():  # put application's code here
+    game_mode = request.args.get('gameMode')
+    player_1_name = request.args.get('player1Name')
+    player_2_name = request.args.get('player2Name')
+
+    game_config = {
+        'game_mode': game_mode,
+        'player_1_name': player_1_name,
+        'player_2_name': player_2_name,
+    }
+
+    if validate_user_input(game_config):
+        return render_template('play.html', game_config=game_config)
+
     return render_template('index.html')
 
 
---
+++ web/static/js/play.js
@@ -0,0 +1,126 @@
+const statusDisplay = document.querySelector('.game--status');
+
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
+
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
+let game_mode = document.getElementById("game_mode_variable").innerHTML.replace(/\s+/g,'');
+let player_1_name = document.getElementById("player_1_name_variable").innerHTML.replace(/\s+/g,'');
+let player_2_name = document.getElementById("player_2_name_variable").innerHTML.replace(/\s+/g,'');
+
+let isSingleModeGame = game_mode === 'SingleMode';
+
+statusDisplay.innerHTML = currentPlayerTurn();
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+
+function getBotRandomStepCellId() {
+    let freeCellIds = [];
+
+    gameState.forEach(function (value, index) {
+        if (value === "") {
+            freeCellIds.push(index);
+        }
+    });
+
+    return 'col' + freeCellIds[Math.floor(Math.random() * freeCellIds.length)].toString()
+
+}
+
+function handlePlayerChange() {
+    if (gameActive) {
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+    }
+
+    if (isSingleModeGame && currentPlayer === "O") {  // BOT step
+        document.getElementById(getBotRandomStepCellId()).click();
+
+        handleResultValidation();
+
+        // Toggle Player
+        if (gameActive) {
+            currentPlayer = 'X';
+        }
+    }
+
+    if (gameActive) {
+        statusDisplay.innerHTML = currentPlayerTurn();
+    }
+}
+
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break
+        }
+    }
+
+    if (roundWon) {
+        statusDisplay.innerHTML = winningMessage();
+        gameActive = false;
+        return;
+    }
+
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
+        return;
+    }
+}
+
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
+
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+    handlePlayerChange();
+}
+
+function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.col').forEach(col => col.innerHTML = "");
+}
+
+function handleReturnMainMenu() {
+    location.href = "/";
+}
+
+document.querySelectorAll('.col').forEach(col => col.addEventListener('click', handleCellClick));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
+document.querySelector('.return-menu').addEventListener('click', handleReturnMainMenu);
